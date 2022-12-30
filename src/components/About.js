ce2171b42ff4d3e630afef23565a2aa771142b51
Darian Fultz <dfultz826@yahoo.com>
2022-12-30 11:45:35 -0500

    Completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id = "about">
+    <h2>About Me</h2>
+    <p>I am currently learning to be a full stack developer.  I like the sound of that!</p>
+    <img src={image} alt = "I made this"/>
+  </div>
+  );
 }
 
 export default About;
